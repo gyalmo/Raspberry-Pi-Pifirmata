331b70f29f67646871de5d06d61ccf92655ef7ff
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2020-08-07 23:11:23 +0530

    Update PyFirmata -2.py

--- PyFirmata -2.py
+++ PyFirmata -2.py
@@ -1,1 +1,15 @@
+#Pyfirmata -2
 
+Import pyfirmata
+Import time
+board = pyfirmata.Arduino (‘dev/ttyACMO’)
+analog _pin = board.get_pin(‘a:1:i’)
+it=pyfirmata.util.Iterator(board)
+it.start()
+analog_pin.enable_reporting()
+while True:
+  reading = analog_pin.read()
+  If reading!=None:
+     voltage = reading = 5.0
+     print (“Reading = %f\t voltage= %f%(reading,Voltage))
+time.sleep(1)
